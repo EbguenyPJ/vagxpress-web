119cbd05dcc548eec1e9aa68965f583fad030732
migueargu <miguelargueta2351@hotmail.com>
2025-12-19 15:59:16 -0600

    Cambios 19/dic/2025

--- src/app/pages/operaciones/cortes/dialogs/dialog-nuevo-corte/dialog-nuevo-corte.component.ts
+++ src/app/pages/operaciones/cortes/dialogs/dialog-nuevo-corte/dialog-nuevo-corte.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-dialog-nuevo-corte',
-  imports: [],
+  imports: [MatIcon,  MatDialogModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './dialog-nuevo-corte.component.html',
   styleUrl: './dialog-nuevo-corte.component.scss'
 })
